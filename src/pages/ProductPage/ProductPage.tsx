af7ba1151bc0643eac6323149b1675681f7cf423
sotakeiteasy <murasov96@gmail.com>
2025-02-11 17:10:54 +0300

    change productPage
    - изменил карусель с горизонтальной на вертикальную: в горизонтальной
      нет смысла при наличии свободного места по краям от основной
      фотографии, приходится пролистывать страницу, чтобы получить к ней
      доступ
    - переделал механику карусели, чтобы работала по кнопкам и обычному
      тапу, добавил обводку для выбранной фотографии
    - убрал серый фон productCard и описания - выглядит дешево, для товара
      позиционируемого как около-премиальный (нат. кожа) выглядит нелогично
    - линиям в div-разделителях задал размер через scale, иначе там пиксели
      рендерятся разной толщины на большой диагонали
    - в описании свернул большинство характеристик, так как они не особо
      полезны для первого впечатления, а визуально захламляют
    - изменил логику на мобильной версии, так как карусель вертикальная, пока что
      сделал карусель+фото и характеристики под ними, на совсем  маленьком экране карусель
      отключается, надо добавить там перелистывание просто по фото

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,17 +1,56 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { ModalWindow, TProduct } from '../../lib';
 import MCParseArticle from '../../lib/uikit/components/MCParseArticle/MCParseArticle';
 import styles from './ProductPage.module.scss';
 import { Image } from 'antd';
 
+import { useRef } from 'react';
+
+
 interface TProps {
 	product: TProduct;
 }
 
+
 const ProductPage: FC<TProps> = ({ product }) => {
 	const [showModal, setShowModal] = useState<boolean>(false);
 	const [chosenImage, setChosenImage] = useState<string>(product.images[0]);
+	
+	const [isOpen, setIsOpen] = useState(false);
+	const carouselRef = useRef<HTMLDivElement>(null);
+	const delta = 2;
+	const [position, setPosition] = useState(0)
+
+	const targetSpecs = ["Материал", "Тип", "Пол"]; // Выбираем характеристики, которые будут отображаться приоритетно - остальные скрыты
+
+	const scrollToImage = (delta: number) => {
+		carouselRef.current?.scrollBy({ top: 130 * delta, behavior: 'smooth' });
+	};
+
+	const scrollUp = () => {
+		scrollToImage(-delta);
+	};
+
+	const scrollDown = () => {
+		scrollToImage(delta); 
+	};
+
+	useEffect(() => {
+		const carouselElement = carouselRef.current;
+
+		if (!carouselElement) return;
+
+		const onScroll = () => {
+			const scrollTop = carouselElement.scrollTop;
+			const newPosition = scrollTop / 65;
+			setPosition(newPosition);
+		};
+
+		carouselElement.addEventListener('scroll', onScroll);
+		return () => carouselElement.removeEventListener('scroll', onScroll);
+	}, []); 
+
 
 	return (
 		<div className={styles.ProductPage}>
@@ -30,60 +69,78 @@ const ProductPage: FC<TProps> = ({ product }) => {
 			/>
 			<div className={styles.innerBox}>
 				<div className={styles.productCard}>
-					<h3 className={styles.title}>{product.title}</h3>
 					<div className={styles.productInfo}>
+
+						<div className={styles.carouselContainer}>
+							<button className={styles.carouselButtonTop} onClick={scrollUp} disabled={position <= 0}>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<title>chevron-up</title>
+									<path d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" />
+								</svg>
+							</button>
+							<div className={styles.imagesCarousel} ref={carouselRef}>
+								{product.images.map((image) => (
+									<img
+										onClick={() => setChosenImage(image)}
+										src={image}
+										className={`${styles.carouselImage} ${chosenImage === image ? styles.active : ''}`}
+
+										key={image}
+									/>
+								))}
+							</div>
+							<button className={styles.carouselButtonBottom} onClick={scrollDown} disabled={position >= product.images.length }>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<title>chevron-down</title>
+									<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+							</button>
+						</div>
 						<div className={styles.imageWrapper}>
 							<Image
 								className={styles.image}
 								src={chosenImage}
 								alt={product.title}
 							/>
-							<div className={styles.imagesCarousel}>
-								{product.images.map((image) => (
-									<img
-										onClick={() => setChosenImage(image)}
-										src={image}
-										className={styles.carouselImage}
-										key={image}
-									/>
-								))}
-							</div>
 						</div>
 						<div className={styles.productSpecs}>
+							<h3 className={styles.title}>{product.title}</h3>
 							<ul className={styles.productSpecsList}>
-								{product?.characteristics?.map(
-									(spec, index) => (
-										<li
-											className={styles.productSpecItem}
-											key={spec.name + index}
-										>
+								
+								{targetSpecs.map((target) => {
+								const spec = product?.characteristics?.find((item) => item.name === target);
+								return spec ? (
+								<li className={styles.productSpecItem} key={spec.name}>
+									<div className={styles.productSpec}>
+									<div className={styles.productSpecName}>
+										<p className={styles.label}>{spec.name}</p>
+										<div className={styles.specDivider} />
+									</div>
+									<p className={styles.productSpecValue}>{spec.value}</p>
+									</div>
+								</li>
+								) : null;
+							})}
+							</ul>
+							<button className={isOpen ? styles.sectionButtonHidden : styles.sectionButton} onClick={() => setIsOpen(!isOpen)}>
+								{isOpen ? "Скрыть" : "Подробнее"}
+							</button>
+							{isOpen && (
+								<ul className={styles.productSpecsList}>
+									{product?.characteristics
+										?.filter((spec) => !targetSpecs.includes(spec.name))
+										.map((spec) => (
+										<li className={styles.productSpecItem} key={spec.name}>
 											<div className={styles.productSpec}>
-												<div
-													className={
-														styles.productSpecName
-													}
-												>
-													<p className={styles.label}>
-														{spec.name}
-													</p>
-													<div
-														className={
-															styles.specDivider
-														}
-													/>
-												</div>
-												<p
-													className={
-														styles.productSpecValue
-													}
-												>
-													{spec.value}
-												</p>
+											<div className={styles.productSpecName}>
+												<p className={styles.label}>{spec.name}</p>
+												<div className={styles.specDivider} />
+											</div>
+											<p className={styles.productSpecValue}>{spec.value}</p>
 											</div>
 										</li>
-									)
-								)}
-							</ul>
+									))}
+								</ul>
+							)}
 							{/* <div className={styles.productSpecsFooter}>
 								<div className={styles.productOrder}>
 									{product.price && (
